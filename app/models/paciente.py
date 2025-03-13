cd979ddb484b1d5c52956f50d1126b30b140d323
dimas <dimasalpaiva@gmail.com>
2025-03-13 08:06:31 -0300

    feat: melhora estrutura de dados, usa uuid ao invés de inteiros

--- app/models/paciente.py
+++ app/models/paciente.py
@@ -1,11 +1,12 @@
 import datetime
 
-from sqlalchemy import Integer, String, Date
+from sqlalchemy import Integer, UUID, String, Date
 from sqlalchemy.orm import Mapped, mapped_column
+from uuid import uuid4
 from ..database import db
 
 class Paciente(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     nome: Mapped[str] = mapped_column(String(100), nullable=False)
     cpf: Mapped[str] = mapped_column(String(14), unique=True, nullable=False)
     data_nascimento: Mapped[datetime.datetime] = mapped_column(Date, nullable=False)
